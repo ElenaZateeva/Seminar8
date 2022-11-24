f32e2254c6240806664537bff80d4ad345152055
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-24 13:24:53 +0300

    Поменять местами строки и столбцы в 2х мерном рандомном массиве

---
+++ Exam055/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 55: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами строки на столбцы.*/
+
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+int[,] result=ChangeColumnAndRow(array);
+Console.WriteLine();
+Print2DArray(result);
+
+int[,] ChangeColumnAndRow(int[,] array)
+{
+    int[,] array2=new int [array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array2[j,i]=array[i,j];
+        }
+    }
+    return array2;
+}
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
