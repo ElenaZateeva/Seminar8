df6347a2eee7f676eea0220e862495da0c44dab0
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-24 18:23:28 +0300

    Отсортировать по убыванию строки в 2х мерном массиве

---
+++ Exam054/Program.cs
@@ -0,0 +1,77 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите метод, который упорядочит 
+по убыванию элементы каждой строки двумерного массива. И продемонстрируйте 
+его работу. Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+Console.WriteLine();
+int[,] result = SortArrayRow(array);
+Print2DArray(result);
+
+int[,] SortArrayRow(int[,] array)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int l = j; l < array.GetLength(1); l++)
+            {
+                int temp = 0;
+                if (result[i, j] < array[i, l])
+                {
+                    temp = result[i, j];
+                    result[i, j] = array[i, l];
+                    array[i, l] = temp;
+                }
+            }
+
+
+        }
+    }
+    return result;
+}
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
