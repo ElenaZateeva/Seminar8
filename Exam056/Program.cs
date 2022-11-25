6e667d24d12c68a58d3556e3351b10327287f0c7
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-25 23:37:36 +0300

    Найти строку массива, сумма элементов которой минимальна

---
+++ Exam056/Program.cs
@@ -0,0 +1,64 @@
+﻿/* задача 56. Задайте двумерный массив. Напишите метод, 
+который будет находить строку с наименьшей суммой элементов.*/
+
+Console.WriteLine("Введите m - ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите n - ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+int row=MinSumRowArray(array);
+Console.WriteLine($"Минимальная сумма сроки под номером {row}");
+
+int MinSumRowArray(int[,] array)
+{
+    int minSum=1000000;
+    int row=-1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum+=array[i,j];
+        }
+        if (minSum>sum)
+        {
+            minSum=sum;
+            row=i;
+        }
+    }
+    return row;
+}
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (var i = 0; i < array.GetLength(0); i++)
+    {
+        for (var j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
